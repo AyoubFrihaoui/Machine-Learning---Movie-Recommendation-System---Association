cf349d3f055b03684b31be574fc41dda215bf6bb
mohamed el amine <serradjmohamed55@gmail.com>
2024-01-11 09:09:16 +0100

    the recommndation api

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI,Body,Request
+from model_import import recommendations_name 
+
+app=FastAPI()
+
+
+@app.post("/users")
+async def get_recommendation(request:Request,movie : str= Body(...,embed=True) ):
+    requestBody=await request.body()
+    content= requestBody.decode()
+    print(content)
+    recommendations = recommendations_name(movie)
+    return {"recommendations":recommendations}
+
+
+
