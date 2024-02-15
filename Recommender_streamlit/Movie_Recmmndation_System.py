17939b6e021a52c95f8be78cec19b7781867e784
frihaoui ayoube <a.frihaoui@esi-sba.dz>
2024-02-15 03:27:29 +0100

    added style fix

--- Recommender_streamlit/Movie_Recmmndation_System.py
+++ Recommender_streamlit/Movie_Recmmndation_System.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="movie recommendation system ",layout="wide")
 
-with open('style.css') as f:
+with open('./style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     
     
